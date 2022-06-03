dd6f6c27dcb65295becb9860f1556413b04dc8ba
Shubh Saxena <shubhsaxena2001@gmail.com>
2022-06-03 11:17:37 +0530

    Second Commit

--- calculator.js
+++ calculator.js
@@ -1,33 +1,28 @@
-const express= require("express");
-const bodyParser = require("body-parser")
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}))
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", (req, res) => {
+  let n1 = Number(req.body.num1);
+  let n2 = Number(req.body.num2);
+  let n3 = n1 + n2;
+  res.send("Thank you for your response. The sum is " + n3);
+});
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname+"/index.html")
-})
-app.post("/", (req, res)=>{
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmicalculator", (req, res) => {
+  let weight = Number(req.body.weight);
+  let height = Number(req.body.height);
+  let bmi = weight / (height * height);
+  res.send("Thank you for your response. Your calculated bmi is " + bmi);
+});
 
-    let n1= Number(req.body.num1);
-    let n2= Number(req.body.num2);
-    let n3 = n1+n2;
-    res.send("Thank you for your response. The ans is " + n3)
-})
-
-
-app.get("/bmicalculator", (req, res)=>{
-    res.sendFile(__dirname+"/bmiCalculator.html")
-})
-app.post("/bmicalculator", (req, res)=>{
-    let weight = Number(req.body.weight)
-    let height = Number(req.body.height)
-    let bmi = weight/(height*height)
-    res.send("Your calculated bmi is "+ bmi)
-})
-
-
-app.listen(3000, ()=>{
-    console.log("Server Started at Port 3000");
-})
-
+app.listen(3000, () => {
+  console.log("Server Started at Port 3000");
+});
